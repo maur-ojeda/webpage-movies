98e399e7a9269136198324eb1a3c33a1b6869ca4
maur-ojeda <49486702+maur-ojeda@users.noreply.github.com>
2022-04-18 11:47:43 -0400

    change source json

--- src/App.js
+++ src/App.js
@@ -20,15 +20,35 @@ const loadMovies = () => {
   movies = movies.slice(
     (currentPage - 1) * total_for_page ,
      currentPage + total_for_page
-     )
-  
+     )  
 }
  
-   const getTotalPages = () => {
-      
-    let totalMovies = Movies.length / total_for_page
-    return Math.ceil(totalMovies)
-   }
+const getTotalPages = () => {
+let totalMovies = Movies.length / total_for_page
+return Math.ceil(totalMovies)
+}
+
+const searchMovies = () => {
+  let url = 'https://raw.githubusercontent.com/maur-ojeda/webpage-movies/6b61ce86b4e5954a8433608f6d869c5645567ebc/src/movies.json'
+
+// using fetch
+  var result = fetch( url, {
+    "method": "GET",
+    "headers": {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+    }).then(
+      result => {
+        alert(result)
+        return result
+      }
+    )
+
+var refult 
+
+}
+
 
    loadMovies();
   return (
